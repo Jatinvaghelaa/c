5fc2462b40132d59671dc72fed1c985152ed8c04
Jatinvaghelaa <126477293+Jatinvaghelaa@users.noreply.github.com>
2023-03-25 17:58:22 +0530

    Add files via upload

---
+++ jatin.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+  printf("name : jaitn vaghela\n");
+  printf("birth date: 19 2 2004\n");
+  printf("age :19\n");
+  printf("address : gandhigram jivantika nagar main road rajkot\n");
+  
+}
